03118f2e22d449700f5123f82e5e003b86e572b5
Pedro Piñera <pedro@ppinera.es>
2018-12-16 12:45:54 +0100

    Remove twitter initializer

--- config/initializers/twitter.rb
+++
@@ -1,8 +0,0 @@
-module Twitter
-  Client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = Figaro.env.twitter_consumer_key!
-    config.consumer_secret     = Figaro.env.twitter_consumer_secret!
-    config.access_token        = Figaro.env.twitter_access_token!
-    config.access_token_secret = Figaro.env.twitter_access_secret!
-  end
-end
